0f32210d218b22d6dfdd26f45c887b1b706c9dd0
Marlon Berdefy <berdefymarlon@yahoo.de>
2017-08-25 12:21:11 +0200

    fuck that datepicker I won't do it

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,9 @@
 //= require bootstrap-sprockets
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
+//= require bootstrap-datepicker
 //= require attachinary
 //= require underscore
 //= require gmaps/google
 //= require_tree .
+
