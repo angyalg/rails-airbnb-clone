6ace84bd771917c7d3548cb8c5b9c868a91e7de0
Vedika Jain <vedikajain.350@gmail.com>
2017-08-25 11:49:17 +0200

    Home animation

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,12 @@
+// All the JS related to the home page
+
+$(window).load(function() {
+  $('.post-module').hover(function() {
+    $(this).find('.description').stop().animate({
+      height: "toggle",
+      opacity: "toggle"
+    }, 300);
+  });
+});
+
+
